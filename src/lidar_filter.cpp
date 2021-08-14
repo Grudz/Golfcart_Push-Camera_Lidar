bca469fae503bf81ada74b45ce788d5d00c0053d
Your Name <you@example.com>
2021-08-14 17:28:37 -0400

    Passthrough XYZ working

--- src/lidar_filter.cpp
+++ src/lidar_filter.cpp
@@ -19,7 +19,6 @@ LidarFilter::LidarFilter(ros::NodeHandle n, ros::NodeHandle pn)
   // Passthrough filter function
   void LidarFilter::recvCloud(const sensor_msgs::PointCloud2ConstPtr& msg)
   {
-
     // Create pointer to PCL type variable
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZI>);
     //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
@@ -29,18 +28,17 @@ LidarFilter::LidarFilter(ros::NodeHandle n, ros::NodeHandle pn)
     pcl::fromROSMsg(*msg, *cloud_in); 
 
     // Start filtering
-    //pcl::IndicesPtr roi_indices(new std::vector <int>); // Will store subset of original array. ROI = region of interest
-    //pcl::PassThrough<pcl::PointXYZI> passthrough_filter; // Instantiate filter. Will find x, y, z points that will satisfy ROI
+    pcl::IndicesPtr roi_indices(new std::vector <int>); // Will store subset of original array. ROI = region of interest
+    pcl::PassThrough<pcl::PointXYZI> passthrough_filter; // Instantiate filter. Will find x, y, z points that will satisfy ROI
 
     // Put pointer to input cloud in passthrough filter
-   //passthrough_filter.setInputCloud(cloud_in);
-    /*
+    passthrough_filter.setInputCloud(cloud_in);
+
     // Index is relative to the Lidar frame
     // Extract X points
     passthrough_filter.setFilterFieldName("x"); // 3 fields, hence PointXYZI
     passthrough_filter.setFilterLimits(cfg_.x_min, cfg_.x_max);
     passthrough_filter.filter(*roi_indices);    // Referes to input cloud
-
     // Extract Y points
     passthrough_filter.setIndices(roi_indices);
     passthrough_filter.setFilterFieldName("y"); 
@@ -48,24 +46,38 @@ LidarFilter::LidarFilter(ros::NodeHandle n, ros::NodeHandle pn)
     passthrough_filter.filter(*roi_indices);    
  
     // Extract Z points. 
-    ROS_INFO("Total points in cloud in = %d\n", (int)cloud_in->size());
+    //ROS_INFO("Total points in cloud in = %d\n", (int)cloud_in->size());
     //passthrough_filter.setIndices(roi_indices);
     passthrough_filter.setFilterFieldName("z"); 
     passthrough_filter.setFilterLimits(cfg_.z_min, cfg_.z_max);
-    passthrough_filter.filter(*cloud_in);     // Final stage so pass filter cloud so instead of int array it's a corresponding point cloud  
-    ROS_INFO("Total points in cloud out = %d\n", (int)cloud_out->size());
+    passthrough_filter.filter(*cloud_out);     // Final stage so pass filter cloud so instead of int array it's a corresponding point cloud  
+    //ROS_INFO("Total points in cloud out = %d\n", (int)cloud_out->size());
 
-    // Downsample the cloud with Voxel Grid filter
-    */
-    pcl::VoxelGrid<pcl::PointXYZI> downsample;
-    downsample.setInputCloud(cloud_in);  
-    downsample.setLeafSize(cfg_.voxel_size, cfg_.voxel_size, cfg_.voxel_size); // All same so cube
-    downsample.filter(*cloud_in); 
-    
+    // Copy filtered data into a ROS message
+    sensor_msgs::PointCloud2 filtered_cloud;
+    pcl::toROSMsg(*cloud_out, filtered_cloud);
 
-    
+    filtered_cloud.header = msg->header;
 
+    // Publish Passthrough filter PointCloud
+    pub_cloud_.publish(filtered_cloud);
 
+    /*
+    // Create pointer to PCL type variable
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZI>);
+
+    // From sensor msg to pcl
+    pcl::fromROSMsg(*msg, *cloud_in); 
+   
+    for (int i = 0; i < (int)cloud_in->size(); i++)
+    {
+      double intensity = (double)cloud_in->points.at(i).intensity;
+      //ROS_INFO("Intensity value per point is %f\n", intensity);
+      if (intensity < 50)
+      {
+        cloud_in->points.at(i) = std::numeric_limits<float>::quiet_NaN();
+      }
+    }
 
     // Copy filtered data into a ROS message
     sensor_msgs::PointCloud2 filtered_cloud;
@@ -75,19 +87,20 @@ LidarFilter::LidarFilter(ros::NodeHandle n, ros::NodeHandle pn)
 
     // Publish Passthrough filter PointCloud
     pub_cloud_.publish(filtered_cloud);
+    */
 
   }
 
   void LidarFilter::timerCallback(const ros::TimerEvent& event)
   {
 
-    ROS_WARN("HERE\n");
+    //ROS_WARN("HERE\n");
     
   }
   void LidarFilter::reconfig(GolfcartPushConfig& config, uint32_t level)
   {
 
-    return;
+    cfg_ = config;
 
   }
 
